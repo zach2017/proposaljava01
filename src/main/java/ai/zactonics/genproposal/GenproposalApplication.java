d08fce892f872958fe749b24ff23e3886ce72b52
zach2017 <zaclewis@gmail.com>
2025-09-02 13:37:14 -0500

    a

---
+++ src/main/java/ai/zactonics/genproposal/GenproposalApplication.java
@@ -0,0 +1,26 @@
+package ai.zactonics.genproposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class GenproposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenproposalApplication.class, args);
+	}
+
+	@RestController
+    public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
+
+}
